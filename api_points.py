a2f88036120b87a03489cf50427f9e94974a0593
George Lifchits <george.lifchits@gmail.com>
2013-11-28 00:56:41 -0500

    moved endpoints to config file

---
+++ api_points.py
@@ -0,0 +1,5 @@
+API_BASE = 'https://greenbutton.affsys.com/ldc/api/v1/'
+
+SERVICE_STATUS = '{0}ReadServiceStatus'.format(API_BASE)
+AUTH_STATUS = '{0}ReadAuthorizationStatus'.format(API_BASE)
+GET_EUI = '{0}UsagePoint'.format(API_BASE)
