eb52b409f42d1da9c6a9e23a7835bd603732c010
George Lifchits <george.lifchits@gmail.com>
2013-11-28 19:41:30 -0500

    updated schema a bit

--- schema.py
+++ schema.py
@@ -1,16 +1,12 @@
-from sqlalchemy.engine import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
-
-engine = create_engine('postgresql://localhost', echo=True)
-connection = engine.connect()
 
 Base = declarative_base()
 
 
 class Reading(Base):
-    __tablename__ 'reading_type'
+    __tablename__ 'reading'
 
     id = Column(Integer, primary_key=True)
     title = Column(String)
@@ -30,19 +26,29 @@ class Reading(Base):
     # from ServiceCategory
     service_kind = Column(Integer)
 
-    readings = relationship("MeterReading", order_by='MeterReading.time_start',
-            backref='reading')
 
-
-class MeterReading(Base):
-    __tablename__ = 'meter_reading'
+class Interval(Base):
+    __tablename__ 'interval'
 
     id = Column(Integer, primary_key=True)
-    reading_id = Column(Integer, ForeignKey('reading_type.id'))
-    time_start = Column(Integer)
-    time_duration = Column(Integer)
-    cost = Column(Integer)
+    duration = Column(Integer)
+    start = Column(Integer)
+    reading_id = Column(Integer, ForeignKey('reading.id'))
 
-    reading = relationship("Reading", backref=backref('meter readings',
-        order_by=time_start))
+    reading = relationship("Reading", backref=backref('readings',
+        order_by=id))
+
+
+class IntervalReading(Base):
+    __tablename__ = 'interval_reading'
+
+    id = Column(Integer, primary_key=True)
+    interval_id = Column(Integer, ForeignKey('interval.id'))
+    start = Column(Integer)
+    duration = Column(Integer)
+    cost = Column(Integer)
+    value = Column(Integer)
+
+    interval = relationship("Interval", backref=backref('interval',
+        order_by=id))
 
