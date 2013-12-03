6b9dc7d2dcfd2362165903594f28654f47ba92d0
George Lifchits <george.lifchits@gmail.com>
2013-12-03 01:32:14 -0500

    login required function woooo

--- app/auth.py
+++ app/auth.py
@@ -2,9 +2,22 @@
 from flask import redirect, url_for, render_template, session, request, flash
 from flask import Blueprint, current_app as app
 
+from functools import wraps
+
 from schema import *
+from constants import *
 
 auth = Blueprint('auth', __name__)
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get('user') is None:
+            flash((CSS_ERR, "You must login before you can do that!"))
+            return redirect(url_for('auth.login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -12,7 +25,7 @@ def login():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-        flash("Success!")
+        flash((CSS_SUCC, "Success!"))
         return redirect(url_for('home'))
 
     return render_template('login.html')
@@ -21,7 +34,19 @@ def login():
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        flash('Success!')
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        try:
+            user = User()
+            user.name = name
+            user.email = email
+            user.set_password(password)
+            flash((CSS_SUCC, 'Success!'))
+        except Exception as e:
+            flash((CSS_ERR, str(e)))
+
         return redirect(url_for('home'))
 
     return render_template('register.html')
