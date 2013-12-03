1efc8af11fb9ac499588f6cac7686fdcf6195737
George Lifchits <george.lifchits@gmail.com>
2013-12-03 01:51:59 -0500

    hopefully more foolproof after db reset

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 import os
+import schema
 
 app = Flask(__name__)
 
 app.debug = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost'
 db = SQLAlchemy(app)
+
+db.create_all()
 
 app.secret_key = os.urandom(24)
