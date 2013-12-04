a656806d9d5ba5f287802c4ec551497b78d6138b
George Lifchits <george.lifchits@gmail.com>
2013-12-03 21:19:42 -0500

    login required config

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,10 @@ from flask.ext.login import LoginManager
 
 import os
 import schema
+import constants
+
 
 app = Flask(__name__)
-
-lm = LoginManager()
-lm.init_app(app)
 
 app.debug = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost'
@@ -17,3 +16,9 @@ db = SQLAlchemy(app)
 db.create_all()
 
 app.secret_key = os.urandom(24)
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'auth.login'
+lm.login_message = (constants.CSS_ERR, "You need to be logged in to do that!")
+
