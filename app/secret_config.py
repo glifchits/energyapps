9240e4a24f53f2aac516a0e7bf648298b4102bba
George Lifchits <george.lifchits@gmail.com>
2013-12-08 21:58:10 -0500

    this I wont wanna commit in the future

---
+++ app/secret_config.py
@@ -0,0 +1,8 @@
+#'''
+CLIENT_ID = 'id-9e3fb855-0df1-40a0-8304-ad4bebfe07b5'
+CLIENT_SECRET = 'secret-595bdb22-004d-4801-8441-a87b38624f45'
+'''
+CLIENT_ID = 'id-a3a5e887-9717-46df-b2f1-7a33ed9da42b'
+CLIENT_SECRET = 'secret-9e586bf6-dd1f-4ea3-9a63-afec930fdad5'
+#'''
+REDIRECT_URI = 'http://localhost:5000/auth'
