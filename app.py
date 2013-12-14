7bd7090e36e0b60736033d577a9e90de7de42aa9
George Lifchits <george.lifchits@gmail.com>
2013-12-14 12:55:05 -0500

    user multilevel authentication

--- app.py
+++ app.py
@@ -17,18 +17,22 @@ app.register_blueprint(charts)
 @app.before_request
 def before_request():
     g.user = current_user
-
     if g.user.is_authenticated() and 'access_token' not in session:
         access_token = g.user.access_token
         if access_token:
             session['access_token'] = access_token
-        app.logger.info('added access token (user %s)' % g.user)
+            app.logger.info('added access token (user %s)' % g.user)
 
 
 @app.route('/')
 def home():
-    if g.user.is_authenticated():
-        return render_template('dashboard.html')
+    if g.user.get_id():
+        if g.user.has_eui():
+            app.logger.debug('user is active, showing dashboard')
+            return render_template('dashboard.html')
+        app.logger.debug('user is logged in but inactive, go to get eui')
+        return render_template('agree_eui.html')
+    app.logger.debug('not logged in')
     return render_template('landing.html')
 
 
