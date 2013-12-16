eb52610080d73c1aa6a6a7539f603fdf98158fa2
George Lifchits <george.lifchits@gmail.com>
2013-12-16 00:13:58 -0500

    a bunch of dashboard info

--- app/data.py
+++ app/data.py
@@ -12,6 +12,9 @@ import cStringIO
 import schema
 db = schema.db
 
+# The EUI returns cost as a multiple of this number
+# divide any cost by this to get an accurate cost
+COST_MULTIPLIER = 100000
 
 data = Blueprint('data', __name__, url_prefix='/data')
 
@@ -232,10 +235,13 @@ def datetime_string(t):
 @login_required
 def dashboard():
     dash_data = {}
-    now = datetime.datetime.now()
+    now = datetime.datetime.now() - datetime.timedelta(days=365)
     day = datetime.timedelta(days=1)
+    week = datetime.timedelta(days=7)
     today_midnight = datetime.datetime(now.year, now.month, now.day)
     yesterday_midnight = today_midnight - day
+
+    owner_id = g.user.get_id()
 
     # today's usage so far
     sql_today = """
@@ -243,20 +249,96 @@ def dashboard():
     from data_view
     where owner = {owner_id}
     and start >= '{start}'::date
+    and start < '{end}'::date
     """.format(
-        owner_id = g.user.get_id(),
-        start = datetime_string(today_midnight)
+        owner_id = owner_id,
+        start = datetime_string(today_midnight),
+        end = datetime_string(now + day)
     )
     rows = db.engine.execute(sql_today)
-    print 'executing sql\n%s' % sql_today
-    print 'here are the rows'
-    for r in rows: print r
+    today_sum_value, today_sum_cost = rows.first()
 
-    dash_data['todaySoFar'] = 1.43
-    dash_data['yesterdayUsage'] = 2325
-    dash_data['dailyAverage'] = 2533
-    dash_data['weeklyUsage'] = 21652
-    dash_data['weeklyAverage'] = 20252
+    # yesterday's usage
+    sql_yesterday = """
+    select sum(value), sum(cost)
+    from data_view
+    where owner = {owner_id}
+    and start >= '{start}'::date
+    and start < '{end}'::date
+    """.format(
+        owner_id = owner_id,
+        start = datetime_string(yesterday_midnight),
+        end = datetime_string(today_midnight)
+    )
+    rows = db.engine.execute(sql_yesterday)
+    yesterday_sum_value, yesterday_sum_cost = rows.first()
+
+    # avg daily
+    sql_avg_daily = """
+    -- select the average over all day's summed energy usage
+    select
+        avg(value) as value,
+        avg(cost) as cost
+    from (
+        -- select the sum of each day's energy usage
+        select
+            min(id) as id,
+            min(start) as start,
+            sum(value) as value,
+            sum(cost) as cost
+        from data_view
+        where owner = {owner_id}
+        group by year, month, day
+    ) as avg_by_day
+    """.format(owner_id = owner_id)
+    rows = db.engine.execute(sql_avg_daily)
+    daily_avg_value, daily_avg_cost = rows.first()
+
+    # last week
+    sql_last_week = """
+    select
+        sum(cost) as cost,
+        sum(value) as value
+    from data_view
+    where owner = {owner_id}
+    and start >= '{start}'::date
+    and start < '{end}'::date
+    """.format(
+        owner_id = owner_id,
+        start = datetime_string(today_midnight - week),
+        end = datetime_string(today_midnight)
+    )
+    rows = db.engine.execute(sql_last_week)
+    last_week_cost, last_week_value = rows.first()
+
+    # avg weekly
+    sql_avg_weekly = """
+    select
+        avg(value) as value,
+        avg(cost) as cost
+    from (
+        select
+            min(id) as id,
+            min(start) as start,
+            sum(value) as value,
+            sum(cost) as cost,
+            div(cast(day as integer), 7) as week
+        from data_view
+        where owner = {owner_id}
+        -- can't include week 4, half weeks
+        and day < 28
+        group by year, month, week
+    ) as avg_by_week
+    """.format(owner_id = owner_id)
+    rows = db.engine.execute(sql_avg_weekly)
+    weekly_avg_value, weekly_avg_cost = rows.first()
+
+
+    dash_data['todaySoFar'] = float( today_sum_cost ) / COST_MULTIPLIER
+    dash_data['yesterdayUsage'] = float ( yesterday_sum_value )
+    dash_data['dailyAverage'] = float( daily_avg_value )
+    dash_data['weeklyUsage'] = float( last_week_value )
+    dash_data['weeklyAverage'] = float( weekly_avg_value )
     dash_data['sleepingUsage'] = 30
     return json.dumps(dash_data)
 
