85b1e7bf3fcb2f8e33695d53819ec68007a4dc6b
George Lifchits <george.lifchits@gmail.com>
2013-12-16 23:36:49 -0500

    formatting attributes

--- app/static/js/dashboard/main.js
+++ app/static/js/dashboard/main.js
@@ -1,12 +1,20 @@
 
-var Widget = function(title, text1, text2, params) {
+var Widget = function(title, text1, text2, params, type) {
     var self = this;
-    self.title = title;
-    self.text1 = ''
-    self.params = params || '';
+    self.title = title + ",";
+    self.text1 = text1;
+    self.text2 = text2;
+    self.params = params;
+    self.type = type;
     self.value = ko.observable();
     self.displayValue = ko.computed(function() {
-        return Math.abs(self.value()).toFixed(2);
+        str = ''; num = self.value();
+        if (self.type === "dollar") {
+            str += "$";
+            num = num / 100000;
+        };
+        str += Math.abs(num).toFixed(2);
+        return str
     });
     self.cssClass = ko.computed(function() {
         return self.value() > 0 ? "bad" : "good"
@@ -27,7 +35,13 @@ var WidgetsViewModel = function() {
     var self = this;
 
     self.widgets = ko.observableArray([
-        new Widget("Today", "grp=day")
+        new Widget(
+            "Today", 
+            "your energy use has cost",
+            "so far",
+            "grp=day",
+            "dollar"
+        ),
     ]);
 };
 
