0efbfb46d52972e03d1900e626fa0a6fbb26fe34
George Lifchits <george.lifchits@gmail.com>
2013-12-20 00:01:25 -0500

    include nvd3 and d3

--- app/static/scripts/dashboard_main.js
+++ app/static/scripts/dashboard_main.js
@@ -2,10 +2,13 @@ require.config({
     baseUrl: '/static/scripts',
     paths: {
         'knockout': '/static/vendor/knockout/knockout',
-        'jquery': '/static/vendor/jquery/jquery'
-    }
+        'jquery': '/static/vendor/jquery/jquery',
+        'nv': '/static/vendor/nvd3/nv.d3',
+        'd3': '/static/vendor/d3/d3'
+    },
 });
 
-require(['knockout', 'jquery', 'dashboard/widgetView'], function(ko, $, WidgetsViewModel) {
+require(['knockout', 'jquery', 'd3', 'nv', 'dashboard/widgetView'], 
+        function(ko, $, d3, nv, WidgetsViewModel) {
     ko.applyBindings(new WidgetsViewModel());
 });
