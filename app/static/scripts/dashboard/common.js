4581c25a75247969f7e6df15ba313b1f064636c0
George Lifchits <george.lifchits@gmail.com>
2013-12-23 03:01:36 -0500

    meter text; meter styles

--- app/static/scripts/dashboard/common.js
+++ app/static/scripts/dashboard/common.js
@@ -9,27 +9,42 @@ define(['jquery-ui'], function() {
         var valuePct = (value - min) / (max - min);
         var goalPct = (goal - min) / (max - min);
 
-        var width = 200,
-            height = 30
+        var message = this.attributes.text.value;
+
+        var width = 400,
+            height = 36;
 
         var svg = d3.select(this).append("svg")
             .attr('width', width)
             .attr('height', height)
-          .append('g')
+          .append('g');
+
+        var background = svg.append("rect")
+            .attr('class', 'background')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('width', width)
+            .attr('height', height);
 
         var goal = svg.append("rect")
             .attr('class', 'goal')
             .attr('x', 0)
             .attr('y', 0)
             .attr('width', goalPct * width)
-            .attr('height', height)
+            .attr('height', height);
 
         var value = svg.append("rect")
             .attr('class', 'value')
             .attr('x', 0)
             .attr('y', 0)
             .attr('width', valuePct * width)
-            .attr('height', height)
+            .attr('height', height);
+
+        msgPadding = 5;
+        var message = svg.append("text")
+            .text(message)
+            .attr('x', msgPadding)
+            .attr('y', height - msgPadding)
 
     });
 
