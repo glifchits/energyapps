19d04b1c338d051e984860c773cf3c2f08eaed9b
George Lifchits <george.lifchits@gmail.com>
2013-12-23 16:26:15 -0500

    resizable meter

--- app/static/scripts/dashboard/common.js
+++ app/static/scripts/dashboard/common.js
@@ -1,50 +1,63 @@
 define(['jquery-ui'], function() {
 
     $('#goals .meter').each(function() {
-        var max = parseFloat(this.attributes.max.value);
-        var min = parseFloat(this.attributes.min.value);
-        var value = parseFloat(this.attributes.value.value);
-        var goal = parseFloat(this.attributes.goal.value);
+        var self = this;
+        var max = parseFloat(self.attributes.max.value);
+        var min = parseFloat(self.attributes.min.value);
+        var value = parseFloat(self.attributes.value.value);
+        var goal = parseFloat(self.attributes.goal.value);
 
         var valuePct = (value - min) / (max - min);
         var goalPct = (goal - min) / (max - min);
 
-        var message = this.attributes.text.value;
+        var messageText = self.attributes.text.value;
+        var titleText = self.attributes.title.value;
 
-        var width = 400,
+        var width = self.offsetWidth,
             height = 36;
 
-        var svg = d3.select(this).append("svg")
-            .attr('width', width)
-            .attr('height', height)
-          .append('g');
+        var svg = d3.select(self).append("svg")
+          .append('g')
 
         var background = svg.append("rect")
             .attr('class', 'background')
             .attr('x', 0)
             .attr('y', 0)
-            .attr('width', width)
-            .attr('height', height);
+            .attr('height', height)
 
         var goal = svg.append("rect")
             .attr('class', 'goal')
             .attr('x', 0)
             .attr('y', 0)
-            .attr('width', goalPct * width)
-            .attr('height', height);
+            .attr('height', height)
 
         var value = svg.append("rect")
             .attr('class', 'value')
             .attr('x', 0)
             .attr('y', 0)
-            .attr('width', valuePct * width)
-            .attr('height', height);
+            .attr('height', height)
 
         msgPadding = 5;
         var message = svg.append("text")
-            .text(message)
+            .text(messageText)
+            .attr('y', height - msgPadding)
+
+        var title = svg.append("text")
+            .text(titleText)
             .attr('x', msgPadding)
             .attr('y', height - msgPadding)
+
+        var updateChart = function() {
+            width = self.offsetWidth;
+            background.attr('width', width);
+            goal.attr('width', goalPct * width);
+            value.attr('width', valuePct * width);
+            msgWidth = message[0][0].offsetWidth;
+            message.attr('x', width - msgWidth);
+        };
+
+        $(window).resize(updateChart);
+        updateChart();
 
     });
 
