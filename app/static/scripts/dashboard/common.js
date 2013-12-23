eee0c8eedbb18e827e5568d7b37643768945b047
George Lifchits <george.lifchits@gmail.com>
2013-12-23 02:28:46 -0500

    goal meter with D3 wip

--- app/static/scripts/dashboard/common.js
+++ app/static/scripts/dashboard/common.js
@@ -4,8 +4,33 @@ define(['jquery-ui'], function() {
         var max = parseFloat(this.attributes.max.value);
         var min = parseFloat(this.attributes.min.value);
         var value = parseFloat(this.attributes.value.value);
-        var pct = (value - min) / (max - min) * 100;
-        $(this).progressbar({ value: pct });
+        var goal = parseFloat(this.attributes.goal.value);
+
+        var valuePct = (value - min) / (max - min);
+        var goalPct = (goal - min) / (max - min);
+
+        var width = 200,
+            height = 30
+
+        var svg = d3.select(this).append("svg")
+            .attr('width', width)
+            .attr('height', height)
+          .append('g')
+
+        var goal = svg.append("rect")
+            .attr('class', 'goal')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('width', goalPct * width)
+            .attr('height', height)
+
+        var value = svg.append("rect")
+            .attr('class', 'value')
+            .attr('x', 0)
+            .attr('y', 0)
+            .attr('width', valuePct * width)
+            .attr('height', height)
+
     });
 
 });
