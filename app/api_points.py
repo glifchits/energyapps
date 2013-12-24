6d8e6afb0c2646c70973b0cfed3acc20f25c2069
George Lifchits <george.lifchits@gmail.com>
2013-12-24 17:57:21 -0500

    working with new test bed

--- app/api_points.py
+++ app/api_points.py
@@ -1,6 +1,7 @@
 import secret_config as secret
 
-URL_BASE = 'https://greenbutton.affsys.com'
+#URL_BASE = 'https://greenbutton.affsys.com'
+URL_BASE = 'https://greenbutton.powerinform.com'
 API_BASE = '%s/ldc/api/v1' % URL_BASE
 
 SERVICE_STATUS = '%s/ReadServiceStatus' % API_BASE
@@ -9,7 +10,7 @@ GET_EUI = '%s/UsagePoint' % API_BASE
 CREATE_SUBSCRIPTION = '%s/Subscription' % API_BASE
 
 AUTH_URL = \
-"https://greenbutton.affsys.com/auth/signin.jsp?client_id={0}&redirect_uri={1}&state=0%2Fa7fcb34d-271f-4910-a886-2ad617021cfe"\
-.format(secret.CLIENT_ID, secret.REDIRECT_URI)
+"{0}/auth/signin.jsp?client_id={1}&redirect_uri={2}&state=0%2Fa7fcb34d-271f-4910-a886-2ad617021cfe"\
+.format(URL_BASE, secret.CLIENT_ID, secret.REDIRECT_URI)
 
 TOKEN_URL = '%s/auth/j_oauth_resolve_access_code' % URL_BASE
--- app/secret_config.py
+++ app/secret_config.py
@@ -1,8 +1,13 @@
-#'''
+'''
 CLIENT_ID = 'id-9e3fb855-0df1-40a0-8304-ad4bebfe07b5'
 CLIENT_SECRET = 'secret-595bdb22-004d-4801-8441-a87b38624f45'
 '''
+'''
 CLIENT_ID = 'id-a3a5e887-9717-46df-b2f1-7a33ed9da42b'
 CLIENT_SECRET = 'secret-9e586bf6-dd1f-4ea3-9a63-afec930fdad5'
-#'''
+'''
+
+CLIENT_ID = 'id-b3a3e803-fcd3-45ab-afa4-990e8c0a2acc'
+CLIENT_SECRET = 'secret-93ecc806-5afb-4c82-9846-f6f823b0f180'
+
 REDIRECT_URI = 'http://localhost:5000/auth'
