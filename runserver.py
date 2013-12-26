5d49cb7d13ee6ad6435f883322c0c73a15cae495
George Lifchits <george.lifchits@gmail.com>
2013-12-25 19:31:47 -0500

    runserver prints ready

--- runserver.py
+++ runserver.py
@@ -3,6 +3,9 @@ from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from main import app
 
-http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(5000)
-IOLoop.instance().start()
+
+if __name__ == "__main__":
+    print "tornado server running"
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(5000)
+    IOLoop.instance().start()
