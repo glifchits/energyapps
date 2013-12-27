603097334d51d5c3208f520c9c0ac540efa51b4a
George Lifchits <george.lifchits@gmail.com>
2013-12-27 16:56:48 -0500

    refactored chart into its own object

---
+++ app/static/scripts/dashboard/chartView.js
@@ -0,0 +1,53 @@
+define(['knockout'], function(ko) {
+
+    var Chart = function(chartId, baseurl, params) {
+        var self = this;
+
+        self.graph = new Rickshaw.Graph.Ajax({
+            element: document.querySelector("#" + chartId),
+            height: 200,
+            renderer: 'line',
+            dataURL: baseurl + ".json?" + params + "&agg=avg",
+            onData: function(data) {
+                var dataTransform = data.map(function(d) {
+                    return {
+                        x: new Date(d.start).getTime(),
+                        y: d.value
+                    };
+                });
+                return [{
+                    "color": "steelblue",
+                    "name": "Value",
+                    "data": dataTransform
+                }];
+            },
+            onComplete: function(transport) {
+                var graph = transport.graph;
+                var detail = new Rickshaw.Graph.HoverDetail({
+                    graph: graph,
+                    xFormatter: function(x) { return new Date(x).toDateString(); },
+                    yFormatter: function(y) { return y.toFixed(1) + " kWh"; }
+                });
+                var yAxis = new Rickshaw.Graph.Axis.Y({ graph: graph });
+                yAxis.graph.update();
+            },
+            series: [{ name: "Value", color: "red" }]
+        });
+
+        var resize = function() {
+            var padding = 10;
+            var graphObj = self.graph.graph; // lol! weird properties I don't understand
+            var el = graphObj.element;
+            graphObj.configure({
+                width: el.offsetWidth - padding,
+                height: el.offsetHeight - padding
+            });
+            graphObj.render();
+        };
+
+        window.addEventListener('resize', resize);
+    };
+
+    return Chart;
+
+});
--- app/static/scripts/dashboard/widget.js
+++ app/static/scripts/dashboard/widget.js
@@ -1,4 +1,4 @@
-define(['knockout'], function(ko) {
+define(['knockout', 'dashboard/chartView'], function(ko, Chart) {
     var Widget = function(title, type, measure, baseurl, params, agg2) {
         var self = this;
         self.title = title;
@@ -81,49 +81,7 @@ define(['knockout'], function(ko) {
         };
 
         self.drawChart = function() {
-            var graph = new Rickshaw.Graph.Ajax({
-                element: document.querySelector("#" + self.chartId),
-                height: 200,
-                renderer: 'line',
-                dataURL: baseurl + ".json?" + params + "&agg=avg",
-                onData: function(data) {
-                    var dataTransform = data.map(function(d) {
-                        return {
-                            x: new Date(d.start).getTime(),
-                            y: d.value
-                        };
-                    });
-                    return [{
-                        "color": "steelblue",
-                        "name": "Value",
-                        "data": dataTransform
-                    }];
-                },
-                onComplete: function(transport) {
-                    var graph = transport.graph;
-                    var detail = new Rickshaw.Graph.HoverDetail({
-                        graph: graph,
-                        xFormatter: function(x) { return new Date(x).toDateString(); },
-                        yFormatter: function(y) { return y.toFixed(1) + " kWh"; }
-                    });
-                    var yAxis = new Rickshaw.Graph.Axis.Y({ graph: graph });
-                    yAxis.graph.update();
-                },
-                series: [{ name: "Value", color: "red" }]
-            });
-
-            var resize = function() {
-                var padding = 10;
-                var graphObj = graph.graph; // lol! weird properties I don't understand
-                var el = graphObj.element;
-                graphObj.configure({
-                    width: el.offsetWidth - padding,
-                    height: el.offsetHeight - padding
-                });
-                graphObj.render();
-            };
-
-            window.addEventListener('resize', resize);
+            new Chart(self.chartId, baseurl, params);
         };
         self.update();
     };
