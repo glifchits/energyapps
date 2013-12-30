6bdea681acf2927d0169377def0de9b4484f2cf2
George Lifchits <george.lifchits@gmail.com>
2013-12-30 04:10:01 -0500

    cmd line port select

--- runserver.py
+++ runserver.py
@@ -5,7 +5,12 @@ from main import app
 
 
 if __name__ == "__main__":
-    print "tornado server running"
+    import sys
+    if len(sys.argv) > 1 and sys.argv[1].isdigit():
+        port = int(sys.argv[1])
+    else:
+        port = 5000
+    print "tornado server running on port %s" % port
     http_server = HTTPServer(WSGIContainer(app))
-    http_server.listen(5000)
+    http_server.listen(port)
     IOLoop.instance().start()
