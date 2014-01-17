26ee47b6096eef5d7f93000c70dc617d9dacb1ae
George Lifchits <george.lifchits@gmail.com>
2014-01-17 17:09:33 -0500

    updated config settings

--- app/config.py
+++ app/config.py
@@ -1,10 +1,18 @@
 import os
+import secret_config
+
 
 class Config(object):
     DEBUG = False
     SECRET_KEY = os.urandom(24)
-
+    BASE_URL = 'http://localhost:5000'
+    REDIRECT_URI = BASE_URL + '/auth'
 
 class LocalConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'postgres://localhost'
+
+class ProdConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = secret_config.PROD_DB_URI
+    BASE_URL = 'http://www.mpowr.ca'
