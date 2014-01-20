c2c20a388d20f5874e6722cd7dee7a4085b0d3b6
George Lifchits <george.lifchits@gmail.com>
2014-01-19 23:51:24 -0500

    redirection page, and main cleanup

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import render_template, g, session, redirect, url_for
 from flask.ext.login import current_user
 
 from app import app
-from app import config
 
 from app.api import api
 app.register_blueprint(api)
@@ -10,8 +9,6 @@ from app.auth import auth
 app.register_blueprint(auth)
 from app.data import data
 app.register_blueprint(data)
-from app.charts import charts
-app.register_blueprint(charts)
 
 
 @app.before_request
@@ -32,7 +29,7 @@ def home():
             app.logger.debug('user %s is active' % g.user.get_id())
             if g.user.access_token is None:
                 app.logger.debug('user has no access token, to data custodian')
-                return redirect(url_for('api.to_data_custodian'))
+                return render_template('redirect.html', url=url_for('api.to_data_custodian'))
 
             if g.user.has_eui():
                 app.logger.debug('user has data, showing dashboard')
