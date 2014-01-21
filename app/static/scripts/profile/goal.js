22d1498ddfdc9fadcbab65c037edc8f5ee5ef4ca
George Lifchits <george.lifchits@gmail.com>
2014-01-21 13:33:17 -0500

    ton of goal edit functionality. reset to average, canceling

--- app/static/scripts/profile/goal.js
+++ app/static/scripts/profile/goal.js
@@ -6,6 +6,51 @@ define(['knockout'], function(ko) {
         self.target = ko.observable(0);
         self.scope = ko.observable(null);
 
+        self.avgMonth = 30278;
+        self.avgWeek = 1335;
+
+        self.old = {
+            target: 0,
+            scope: undefined
+        };
+
+        self.setToAverage = function() {
+            if (self.scope() === 'week')
+                self.target(self.avgWeek.toFixed(1));
+            else if (self.scope() === 'month')
+                self.target(self.avgMonth.toFixed(1));
+            else
+                self.target(0);
+        };
+
+        self.resetVisible = ko.computed(function() {
+            switch(self.scope()) {
+                case 'week':  return Math.abs(self.target()-self.avgWeek) > 0.1;
+                case 'month': return Math.abs(self.target()-self.avgMonth) > 0.1;
+                default: return false;
+            };
+        });
+
+        self.cancelChanges = function() {
+            if (self.changed()) {
+                self.target(self.old.target);
+                self.scope(self.old.scope);
+            }
+        };
+
+        self.scope.subscribe(function(newScope) {
+            if (newScope === self.old.scope) {
+                self.target(self.old.target);
+                return;
+            }; 
+            if (newScope === 'month')
+                self.target(self.avgMonth.toFixed(1));
+            else if (newScope === 'week')
+                self.target(self.avgWeek.toFixed(1));
+            else
+                self.target(0);
+        });
+
         self.computeHash = ko.computed(function() {
             /* returns a string that tries to uniquely
              * capture the current state of this object */
@@ -16,11 +61,11 @@ define(['knockout'], function(ko) {
         self.initialState = ko.observable(null);
         // when initialized, set this to the computedHash
 
-        self.changed = function() {
+        self.changed = ko.computed(function() {
             /* returns whether the object has changed since its
              * state was saved (by setting self.initialState) */
             return self.computeHash() !== self.initialState();
-        };
+        });
 
         self.name = ko.computed(function() {
             switch (self.scope()) {
@@ -57,9 +102,14 @@ define(['knockout'], function(ko) {
         };
 
         self.remove = function() {
+            if (self.id() === 0)
+                return;
+
             spinner(true, "Deleting goal...");
             $.post('/data/goals/' + self.id() + '/delete', function(data) {
                 console.log('delete, got data', data);
+            })
+            .complete(function() {
                 spinner(false);
             });
         };
